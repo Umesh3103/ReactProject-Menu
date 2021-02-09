8035efee3d30354856c2ba33ef1d8ce6397e0513
Deora <umesh.deora@capgemini.com>
2021-02-09 15:31:30 +0530

    [Umesh]Added: Menu project

---
+++ src/data.js
@@ -0,0 +1,85 @@
+const data = [
+  {
+    id: 1,
+    img: "https://react-projects-5-menu.netlify.app/images/item-1.jpeg",
+    title: "Buttermilk Pancakes",
+    category: "breakfast",
+    desc:
+      "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+    price: 15,
+  },
+  {
+    id: 2,
+    img: "https://react-projects-5-menu.netlify.app/images/item-2.jpeg",
+    title: "Dinner Double",
+    category: "lunch",
+    desc:
+      "vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats",
+    price: 15,
+  },
+  {
+    id: 3,
+    img: "https://react-projects-5-menu.netlify.app/images/item-3.jpeg",
+    title: "Godzilla Milkshake",
+    category: "shakes",
+    desc:
+      "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
+    price: 15,
+  },
+  {
+    id: 4,
+    img: "https://react-projects-5-menu.netlify.app/images/item-4.jpeg",
+    title: "Country Delight",
+    category: "breakfast",
+    desc:
+      "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut",
+    price: 15,
+  },
+  {
+    id: 5,
+    img: "https://react-projects-5-menu.netlify.app/images/item-5.jpeg",
+    title: "Egg Attack",
+    category: "lunch",
+    desc:
+      "franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up",
+    price: 15,
+  },
+  {
+    id: 6,
+    img: "https://react-projects-5-menu.netlify.app/images/item-6.jpeg",
+    title: "Oreo Dream",
+    category: "shakes",
+    desc:
+      "Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday",
+    price: 15,
+  },
+  {
+    id: 7,
+    img: "https://react-projects-5-menu.netlify.app/images/item-7.jpeg",
+    title: "Bacon Overflow",
+    category: "breakfast",
+    desc:
+      "carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
+    price: 15,
+  },
+  {
+    id: 8,
+    img: "https://react-projects-5-menu.netlify.app/images/item-8.jpeg",
+    title: "American Classic",
+    category: "lunch",
+    desc:
+      "on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut",
+    price: 15,
+  },
+  {
+    id: 9,
+    img: "https://react-projects-5-menu.netlify.app/images/item-9.jpeg",
+    title: "Quarantine Buddy",
+    category: "shakes",
+    desc:
+      "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
+    price: 15,
+  },
+];
+
+export default data;
